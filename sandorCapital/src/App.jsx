78f0b2bc90a0c7d25a555fbe11e0707a4330ae88
matthew <shaggysci3@gmail.com>
2024-07-12 17:58:34 -0400

    inital commit

---
+++ sandorCapital/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider
+} from "react-router-dom";
+import './App.css'
+import ErrorPage from './routes/ErrorPage';
+import Home from './routes/Home';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  
+
+  return (
+    <>
+    <Outlet></Outlet>
+     
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    
+    
+    element: <App/>,
+  errorElement: <ErrorPage/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      }
+      
+      
+    ]
+  }
+  
+  
+
+]);
+
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
+
---
+++ sandorCapital/src/routes/Home.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+const Home = () =>{
+ 
+
+ 
+   
+
+    return(
+      <>
+      <div className="homePage">
+        <h1 style={{color:"black"}}>this is the home page of this Sandor Capital website</h1>
+        <img src="https://th.bing.com/th/id/OIG3.9Sh1ygKJPljDur2gcROY?pid=ImgGn"></img>
+        
+      </div>
+        
+      </>
+    )
+  }
+  export default Home
